514471d4769a8ce67b014f308d482bd5763926ef
sumit-koley0404 <sumitkoley180@gmail.com>
2025-01-29 22:14:17 +0530

    Get the page

--- JavaSeleniumUIAutomation/src/test/java/JavaAutomation/JavaSeleniumUIAutomation/StandAloneTest.java
+++ JavaSeleniumUIAutomation/src/test/java/JavaAutomation/JavaSeleniumUIAutomation/StandAloneTest.java
@@ -1,10 +1,18 @@
 package JavaAutomation.JavaSeleniumUIAutomation;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
 public class StandAloneTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().window().maximize();
+		driver.get("https://rahulshettyacademy.com/client");
 	}
 
 }
