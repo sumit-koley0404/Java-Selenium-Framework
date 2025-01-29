7bd78bef3bf2ffffebc8a01a6863c5f6245e2398
sumit-koley0404 <sumitkoley180@gmail.com>
2025-01-29 21:36:32 +0530

    First Commit

---
+++ JavaSeleniumUIAutomation/src/main/java/JavaAutomation/JavaSeleniumUIAutomation/App.java
@@ -0,0 +1,13 @@
+package JavaAutomation.JavaSeleniumUIAutomation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
