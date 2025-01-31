a93b5863c965e4ea839884768b021c4d1c9988c9
sumit-koley0404 <sumitkoley180@gmail.com>
2025-01-31 21:44:21 +0530

    Refactor is done for Login

---
+++ JavaSeleniumUIAutomation/src/main/java/JavaSeleniumUIAutomation/AbstractComponent/AbstractComponent.java
@@ -0,0 +1,25 @@
+package JavaSeleniumUIAutomation.AbstractComponent;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class AbstractComponent {
+	
+	
+	WebDriver driver;
+	
+	public AbstractComponent(WebDriver driver)
+	{
+		this.driver = driver;
+	}
+	public void waitForElementToAppear(By findBy){
+		
+		WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(5));
+		wait.until(ExpectedConditions.visibilityOfElementLocated(findBy));
+	}
+
+}
---
+++ JavaSeleniumUIAutomation/src/main/java/JavaSeleniumUIAutomation/pageobjects/LandingPage.java
@@ -0,0 +1,42 @@
+package JavaSeleniumUIAutomation.pageobjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import JavaSeleniumUIAutomation.AbstractComponent.AbstractComponent;
+
+public class LandingPage extends AbstractComponent {
+	WebDriver driver;
+	
+	public LandingPage (WebDriver driver){
+		super(driver);
+		this.driver = driver;
+		PageFactory.initElements(driver, this);	
+	}
+	@FindBy(css="#userEmail")
+	WebElement userEmail;
+	@FindBy(css ="#userPassword")
+	WebElement password;
+	@FindBy(css="#login")
+	WebElement submit;
+	By btn = By.cssSelector("#login");
+	
+	public void goTo()
+	{
+		driver.get("https://rahulshettyacademy.com/client");
+		
+	}
+	public void loginApplication(String email,String psw)
+	{
+		waitForElementToAppear(btn);
+		userEmail.sendKeys(email);
+		password.sendKeys(psw);
+		submit.click();
+	}
+	
+	
+
+}
